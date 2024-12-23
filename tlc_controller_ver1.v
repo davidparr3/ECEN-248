69b86e83a4426fdfa050eba636be87e8ef0491f3
davidparr3 <dparr3@tamu.edu>
2024-12-23 12:09:27 -0600

    Update tlc_controller_ver1.v

--- tlc_controller_ver1.v
+++ tlc_controller_ver1.v
@@ -1,23 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/20/2024 06:13:29 PM
-// Design Name: 
-// Module Name: tlc_controller_ver1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+//@DavidParr
 
 
 module tlc_controller_ver1(
